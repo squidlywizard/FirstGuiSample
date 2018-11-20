c54a5741529eaf37a22c67db58a881e1f9debbca
squidlywizard <ztdenna@hotmail.com>
2018-11-20 09:48:11 -0700

    when you cllick the button the colors change

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -2,6 +2,9 @@ package gui.view;
 
 import javax.swing.*;
 import gui.controller.GuiController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FirstPanel extends JPanel
 
@@ -9,15 +12,17 @@ public class FirstPanel extends JPanel
 	private GuiController appController;
 	private JButton myButton;
 	private JLabel myLabel;
+	private SpringLayout appLayout;
 	
 	public FirstPanel(GuiController appController)
 	
 	{
-		super();
+	    super();
 		this.appController = appController;
 		
 		myButton = new JButton("Click");
 		myLabel = new JLabel("words");
+		appLayout = new SpringLayout();
 		
 		setupPanel();
 		setupLayout();
@@ -26,18 +31,32 @@ public class FirstPanel extends JPanel
 	}
 	private void setupPanel()
 	{
+		this.setLayout(appLayout);//Spring layout puts the gui components into a Spring
 		this.add(myButton);
 		this.add(myLabel);
 	}
 	
 	private void setupLayout()
 	{
+		appLayout.putConstraint(SpringLayout.NORTH, myLabel, 74, SpringLayout.NORTH, this);
+	    appLayout.putConstraint(SpringLayout.EAST, myLabel, -86, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.WEST, myButton, 174, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, myButton, -135, SpringLayout.SOUTH, this);
 		
 	}
 	
-	private void setupListeners()
+	private void setupListeners()// what happens when you click a button
 	{
+		myButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent mouseClick) {changeColor();}});
+	}
+	
+	private void changeColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
 		
+		this.setBackground(new Color(red, green, blue));
 	}
 	
 
